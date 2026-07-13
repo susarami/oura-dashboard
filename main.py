516f9c0434649905d2f7d7294e05f22673857eda
Kevin <vukevin457@gmail.com>
2026-07-13 17:53:28 -0400

    created a function to pull data from Oura API

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+from dotenv import load_dotenv
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+load_dotenv()
+token = os.getenv("OURA_TOKEN")
 
+import requests
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def fetch_oura_data(endpoint, start_date, end_date, token):
+    url = f"https://api.ouraring.com/v2/usercollection/{endpoint}"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    params = {"start_date": start_date, "end_date": end_date}
+    response = requests.get(url, headers=headers, params=params)
 
+    if response.status_code != 200:
+        raise Exception(f"Request failed with status code {response.status_code}")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    return response.json().get("data")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+sleep_records = fetch_oura_data("daily_sleep", "2025-07-01", "2026-07-01", token)
+readiness_records = fetch_oura_data("daily_readiness", "2025-07-01", "2026-07-01", token)
+activity_records = fetch_oura_data("daily_activity", "2025-07-01", "2026-07-01", token)
+
+print(len(sleep_records), len(readiness_records), len(activity_records))
